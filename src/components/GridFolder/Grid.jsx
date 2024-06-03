6caf670d3d344dac9d33dac7c8e4b0913048b844
Jonahs17 <jonahsgeorge17@gmail.com>
2024-06-03 19:40:36 +0530

    some minor changes

--- src/components/GridFolder/Grid.jsx
+++ src/components/GridFolder/Grid.jsx
@@ -3,22 +3,8 @@ import Card from "../Card/Card";
 import './grid.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import isWinner from "../../helper/checkWinner.js";
 
-function isWinner(board,symbol){
-    if (board[0] === board[1] && board[1] === board[2] && board[2] === symbol) return symbol;
-    if (board[3] === board[4] && board[4] === board[5] && board[5] === symbol) return symbol;
-    if (board[6] === board[7] && board[7] === board[8] && board[8] === symbol) return symbol;
-    
-    if (board[0] === board[3] && board[3] === board[6] && board[6] === symbol) return symbol;
-    if (board[1] === board[4] && board[4] === board[7] && board[7] === symbol) return symbol;
-    if (board[2] === board[5] && board[5] === board[8] && board[8] === symbol) return symbol;
-    
-    if (board[0] === board[4] && board[4] === board[8] && board[8] === symbol) return symbol;
-    if (board[2] === board[4] && board[4] === board[6] && board[6] === symbol) return symbol;
-    
-
-    return null;
-}
 
 function Grid({ numberOfCards }) {
     const [turn,setTurn]=useState(true);
@@ -49,15 +35,16 @@ function Grid({ numberOfCards }) {
     }
     return (
         <>
+        <h1 className="turn-highlight">TIC-TAC-TOE</h1>
         {winner && 
          (<>
          <button onClick={reset}>RESET</button>
-         <h1 className="turn-highlight">Winner is {winner}</h1>
+         <h2 className="turn-highlight">Winner is {winner}</h2>
          <ToastContainer position="top-center"/>
          </>)
          
         }
-        <h1 className="turn-highlight">Current Turn :{(turn===true)?'o':'x'}</h1>
+        <h2 className="turn-highlight">Current Turn :{(turn===true)?'o':'x'}</h2>
         <div className="grid">
             {board.map((value, idx) => (
                 <Card key={idx} player={value} onPlay={play} index={idx} gameEnd={winner}/>
